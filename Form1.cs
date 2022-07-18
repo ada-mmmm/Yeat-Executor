9fb8cb22ea5ce7c3eddcabe75ead9c7841ee0c47
ada-mmmm <103240971+ada-mmmm@users.noreply.github.com>
2022-07-17 23:46:26 -0700

    Add files via upload
    some are named adams executor because that was the original name but i changed it

--- Form1.cs
+++ Form1.cs
@@ -21,7 +21,11 @@ namespace AdamsExecutor
 
         Point lastPoint;
 
-        public delegate void MyDelegate(string message);
+        private string one = string.Empty;
+
+        Boolean onelmao = true;
+        Boolean two = false;
+        Boolean three = false;
 
         private void button2_Click(object sender, EventArgs e)
         {
@@ -40,8 +44,24 @@ namespace AdamsExecutor
 
         private void button3_Click(object sender, EventArgs e)
         {
-            string textBox = richTextBox1.Text;
-            module.ExecuteScript(textBox);
+            string textBox1 = richTextBox1.Text;
+            string textBox2 = richTextBox2.Text;
+            string textBox3 = richTextBox3.Text;
+            
+
+            if (onelmao == true)
+            {
+                module.ExecuteScript(textBox1);
+            }
+            else if (two == true)
+            {
+                module.ExecuteScript(textBox2);
+            }
+            else if (three == true)
+            {
+                module.ExecuteScript(textBox3);
+            }
+            
         }
 
         private void panel1_MouseDown(object sender, MouseEventArgs e)
@@ -58,22 +78,6 @@ namespace AdamsExecutor
             }
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            string message = "";
-            BeginInvoke(new MyDelegate(DelegateMethod), message);
-        }
-
-        public void DelegateMethod(string message)
-        {
-            richTextBox1.Text = message;
-        }
-
         private void button6_Click(object sender, EventArgs e)
         {
             module.ExecuteScript("loadstring(game:HttpGet(\"https://pastebin.com/raw/CdjvXLpn\"))()");
@@ -81,7 +85,7 @@ namespace AdamsExecutor
 
         private void button7_Click(object sender, EventArgs e)
         {
-            module.ExecuteScript("loadstring(game:HttpGet(\"https://pastebin.com/raw/RmD3qNp7\", true))()");
+            module.ExecuteScript("loadstring(game:HttpGet(\"https://pastebin.com/raw/0MLPL32f\", true))()");
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -96,7 +100,7 @@ namespace AdamsExecutor
 
         private void button9_Click(object sender, EventArgs e)
         {
-            return;
+            module.ExecuteScript("loadstring(game:HttpGet(\"https://raw.githubusercontent.com/TheXploiterYT/scripts/main/ctrlclicktpfornooblol\", true))()");
         }
 
         private void button10_Click(object sender, EventArgs e)
@@ -107,6 +111,59 @@ namespace AdamsExecutor
         private void button11_Click(object sender, EventArgs e)
         {
             return;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            richTextBox1.Visible = true;
+            richTextBox2.Visible = false;
+            richTextBox3.Visible = false;
+
+            onelmao = true;
+            two = false;
+            three = false;
+        }
+
+        private void button12_Click_1(object sender, EventArgs e)
+        {
+            richTextBox1.Visible = false;
+            richTextBox2.Visible = true;
+            richTextBox3.Visible = false;
+
+            onelmao = false;
+            two = true;
+            three = false;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Visible = false;
+            richTextBox2.Visible = false;
+            richTextBox3.Visible = true;
+
+            onelmao = false;
+            two = false;
+            three = true;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            module.ExecuteScript("loadstring(game:HttpGet(\"https://pastebin.com/raw/ptMwyJA8\"))()");
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            module.ExecuteScript("loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()");
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            module.ExecuteScript("loadstring(game:HttpGet(\"https://gist.githubusercontent.com/redninjaawesome/539336c48e73107232933475d8e6120a/raw/f95fcd6db11128acd1a200e8866f806d3a102098/gistfile1.txt\"))()");
         }
     }
 }
